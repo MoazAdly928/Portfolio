fc6b9ed9a8c727bcea324477f0f1adc0a4656424
moazadly <moazdly@gmail.com>
2025-02-08 01:32:21 +0200

    social media Links

--- app/components/homeSection/Socail.jsx
+++ app/components/homeSection/Socail.jsx
@@ -2,13 +2,18 @@ import style from "./home.module.css";
 function Socail() {
   return (
     <div className={`${style.home__social}`}>
-      <a href="" className={`${style.home__social_icon}`} target="_blank">
-        <i className="uil uil-instagram"></i>
-      </a>
-      <a href="" className={`${style.home__social_icon}`} target="_blank">
+      <a
+        href="https://www.linkedin.com/in/moaz-adly-314074284/"
+        className={`${style.home__social_icon}`}
+        target="_blank"
+      >
         <i className="uil uil-linkedin"></i>
       </a>
-      <a href="" className={`${style.home__social_icon}`} target="_blank">
+      <a
+        href="https://github.com/moazadly"
+        className={`${style.home__social_icon}`}
+        target="_blank"
+      >
         <i className="uil uil-github-alt"></i>
       </a>
     </div>
